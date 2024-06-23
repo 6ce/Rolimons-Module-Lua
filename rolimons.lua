753c3717331dc057976c1b2b9074046f789b5742
6ce <86327263+6ce@users.noreply.github.com>
2024-06-22 23:19:31 -0700

    Update rolimons.lua

--- rolimons.lua
+++ rolimons.lua
@@ -77,7 +77,6 @@ end
 
 function rolimons:getMostValuableItems(limit: number?): {{id: number, name: string, abbreviation: string, rap: number}}
 	limit = limit or 10
-	assert(limit <= 100, "'limit' cannot be larger than 100")
 	
 	if not rolimons.updated then
 		rolimons:getItemsAsync()
